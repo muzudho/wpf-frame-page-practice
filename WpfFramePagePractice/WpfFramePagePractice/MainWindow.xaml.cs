3a519005240bc123115be03f832d43d3ca918e4a
むずでょ <muzudho1@gmail.com>
2021-08-31 21:39:37 +0900

    Frame Page

---
+++ WpfFramePagePractice/WpfFramePagePractice/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfFramePagePractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Uri uri = new Uri("/Page1.xaml", UriKind.Relative);
+            frame.Source = uri;
+        }
+    }
+}
